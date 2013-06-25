e2c960a6e74d67abfbc928ccda7d6199ebc0e117
gaolinjie <gaolinjie@gmail.com>
2013-06-25 15:59:53 +0800

    add flat ui

---
+++ handler/topic.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Copyright 2013 tuila.me
+
+import uuid
+import hashlib
+import Image
+import StringIO
+import time
+import json
+import re
+import urllib2
+import tornado.web
+import lib.jsonp
+import pprint
+import math
+import datetime
+
+from base import *
+from lib.variables import *
+from form.topic import *
+from lib.variables import gen_random
+from lib.xss import XssCleaner
+from lib.utils import find_mentions
+from lib.reddit import hot
+
+class IndexHandler(BaseHandler):
+    def get(self, template_variables = {}):
+
+        self.render("index.html", **template_variables)
+
