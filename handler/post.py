33461a0f72a3ddef343d137a52d166ed3c14f4fb
gaolinjie <gaolinjie@gmail.com>
2014-02-24 17:40:58 +0800

    fix thumb crop issue

--- handler/post.py
+++ handler/post.py
@@ -101,9 +101,9 @@ class CreatePostHandler(BaseHandler):
         # crop avatar if it's not square
         thumb_x = int(form.x1.data)
         thumb_y = int(form.y1.data)
-        thumb_w = float(form.x2.data) - float(form.x1.data)
-        thumb_h = float(form.y2.data) - float(form.y1.data)
-        thumb_crop_region = (thumb_x, thumb_y, int(thumb_w), int(thumb_h))
+        thumb_x2 = int(round(float(form.x2.data)))
+        thumb_y2 = int(round(float(form.y2.data)))
+        thumb_crop_region = (thumb_x, thumb_y, thumb_x2, thumb_y2)
         thumb = thumb_origin.crop(thumb_crop_region)
 
         thumb_125x83 = thumb.resize((125, 83), Image.ANTIALIAS)
@@ -113,9 +113,9 @@ class CreatePostHandler(BaseHandler):
 
         thumb2_x = int(form.x3.data)
         thumb2_y = int(form.y3.data)
-        thumb2_w = float(form.x4.data) - float(form.x3.data)
-        thumb2_h = float(form.y4.data) - float(form.y3.data)
-        thumb2_crop_region = (thumb2_x, thumb2_y, int(thumb2_w), int(thumb2_h))
+        thumb2_x2 = int(round(float(form.x4.data)))
+        thumb2_y2 = int(round(float(form.y4.data)))
+        thumb2_crop_region = (thumb2_x, thumb2_y, thumb2_x2, thumb2_y2)
         thumb2 = thumb_origin.crop(thumb2_crop_region)
 
         thumb_355x125 = thumb2.resize((355, 125), Image.ANTIALIAS)
