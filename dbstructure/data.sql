da72feddc703cc787389aa2f2d46e6d16afe8226
gaolinjie <gaolinjie@gmail.com>
2014-02-27 15:08:46 +0800

    update channel

--- dbstructure/data.sql
+++ dbstructure/data.sql
@@ -1,30 +1,30 @@
-INSERT INTO `nav` VALUES (1, 'news', '新闻');
+INSERT INTO `nav` VALUES (1, 'news', '新鲜事');
+
 INSERT INTO `nav` VALUES (2, 'ent', '娱乐');
+
 INSERT INTO `nav` VALUES (3, 'life', '生活');
+
 INSERT INTO `nav` VALUES (4, 'video', '视频');
 
 
-
 INSERT INTO `channel` VALUES (1, 1, 'world', '世界');
-INSERT INTO `channel` VALUES (2, 1, 'business', '商业');
-INSERT INTO `channel` VALUES (3, 1, 'tech', '科技');
-INSERT INTO `channel` VALUES (4, 1, 'sports', '体育');
-INSERT INTO `channel` VALUES (5, 1, 'ideas', '创意');
+INSERT INTO `channel` VALUES (2, 1, 'world', '社会');
+INSERT INTO `channel` VALUES (3, 1, 'ideas', '创意');
+INSERT INTO `channel` VALUES (4, 1, 'ideas', '其它');
 
-INSERT INTO `channel` VALUES (6, 2, 'star', '明星');
-INSERT INTO `channel` VALUES (7, 2, 'movie', '电影');
+INSERT INTO `channel` VALUES (5, 2, 'star', '搞笑');
+INSERT INTO `channel` VALUES (6, 2, 'star', '八卦');
+INSERT INTO `channel` VALUES (7, 1, 'sports', '体育');
 INSERT INTO `channel` VALUES (8, 2, 'music', '音乐');
-INSERT INTO `channel` VALUES (9, 2, 'catoon', '动漫');
 
 
-INSERT INTO `channel` VALUES (10, 3, 'animal', '动物');
-INSERT INTO `channel` VALUES (11, 3, 'travel', '旅游');
-INSERT INTO `channel` VALUES (12, 3, 'cook', '美食');
-INSERT INTO `channel` VALUES (13, 3, 'diy', 'DIY');
-
-INSERT INTO `channel` VALUES (14, 4, 'original', '原创');
-INSERT INTO `channel` VALUES (15, 4, 'business', '影集');
+INSERT INTO `channel` VALUES (9, 3, 'animal', '动物');
+INSERT INTO `channel` VALUES (10, 3, 'travel', '旅游');
+INSERT INTO `channel` VALUES (11, 3, 'cook', '美食');
+INSERT INTO `channel` VALUES (12, 3, 'diy', 'TIPS');
 
 
-
-
+INSERT INTO `channel` VALUES (13, 4, 'movie', '短片');
+INSERT INTO `channel` VALUES (14, 4, 'movie', '电影');
+INSERT INTO `channel` VALUES (15, 4, 'movie', '电视');
+INSERT INTO `channel` VALUES (16, 4, 'catoon', '动漫');
