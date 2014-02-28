aa73bd859c8e6648ef6d5c41e0cd60ad68a18c1a
gaolinjie <gaolinjie@gmail.com>
2014-02-28 14:41:38 +0800

    fix bugs3

--- dbstructure/data.sql
+++ dbstructure/data.sql
@@ -8,12 +8,12 @@ INSERT INTO `nav` VALUES (4, 'video', '视频');
 
 
 INSERT INTO `channel` VALUES (1, 1, 'world', '世界');
-INSERT INTO `channel` VALUES (2, 1, 'world', '社会');
+INSERT INTO `channel` VALUES (2, 1, 'society', '社会');
 INSERT INTO `channel` VALUES (3, 1, 'ideas', '创意');
-INSERT INTO `channel` VALUES (4, 1, 'ideas', '其它');
+INSERT INTO `channel` VALUES (4, 1, 'others', '其它');
 
-INSERT INTO `channel` VALUES (5, 2, 'star', '搞笑');
-INSERT INTO `channel` VALUES (6, 2, 'star', '八卦');
+INSERT INTO `channel` VALUES (5, 2, 'funny', '搞笑');
+INSERT INTO `channel` VALUES (6, 2, 'gossip', '八卦');
 INSERT INTO `channel` VALUES (7, 2, 'sports', '体育');
 INSERT INTO `channel` VALUES (8, 2, 'music', '音乐');
 
@@ -21,10 +21,10 @@ INSERT INTO `channel` VALUES (8, 2, 'music', '音乐');
 INSERT INTO `channel` VALUES (9, 3, 'animal', '动物');
 INSERT INTO `channel` VALUES (10, 3, 'travel', '旅游');
 INSERT INTO `channel` VALUES (11, 3, 'cook', '美食');
-INSERT INTO `channel` VALUES (12, 3, 'diy', 'TIPS');
+INSERT INTO `channel` VALUES (12, 3, 'tips', 'TIPS');
 
 
-INSERT INTO `channel` VALUES (13, 4, 'movie', '短片');
+INSERT INTO `channel` VALUES (13, 4, 'clip', '短片');
 INSERT INTO `channel` VALUES (14, 4, 'movie', '电影');
-INSERT INTO `channel` VALUES (15, 4, 'movie', '电视');
+INSERT INTO `channel` VALUES (15, 4, 'tv', '电视');
 INSERT INTO `channel` VALUES (16, 4, 'catoon', '动漫');
