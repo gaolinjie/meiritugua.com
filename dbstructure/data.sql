63f2165b6f2599316747a152d4d4a15e05237daa
gaolinjie <gaolinjie@gmail.com>
2014-03-17 17:03:33 +0800

    temp

--- dbstructure/data.sql
+++ dbstructure/data.sql
@@ -2,13 +2,16 @@ INSERT INTO `nav` VALUES (1, 'news', '新鲜事');
 
 INSERT INTO `nav` VALUES (2, 'ooxx', 'OX图');
 
-INSERT INTO `nav` VALUES (3, 'fls', '福利社');
+INSERT INTO `nav` VALUES (3, 'fuli', '福利社');
 
-INSERT INTO `nav` VALUES (4, 'sq', '色区');
+INSERT INTO `nav` VALUES (4, 'bbs', '色区');
 
-INSERT INTO `channel` VALUES (1, 1, 'girls', '女人');
+INSERT INTO `channel` VALUES (1, 1, 'girl', '女人');
 INSERT INTO `channel` VALUES (2, 1, 'life', '生活');
 INSERT INTO `channel` VALUES (3, 1, 'gossip', '八卦');
 INSERT INTO `channel` VALUES (4, 1, 'video', '视频');
 
-INSERT INTO `channel` VALUES (5, 2, 'oximg', 'OX图');
+INSERT INTO `channel` VALUES (5, 2, 'lmz', '撸妹纸');
+INSERT INTO `channel` VALUES (6, 2, 'wjc', '无节操');
+
+INSERT INTO `channel` VALUES (7, 3, 'sex', '性福');
